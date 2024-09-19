7efaaf3f96db6fa33ea41835adc7fdb4c60c49b2
lukaalexandra <lenimide@gmail.com>
2024-09-19 15:11:13 +0500

    after checking

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import Stars from "./components/Stars/Stars"
-import './App.css'
+import Stars from "./components/Stars/Stars";
+import "./App.css";
 
 function App() {
- return (
-  <Stars count={5}/>
-);
+  const films = [
+    { id: 1, name: "Супермен", count: 5 },
+    { id: 2, name: "Одинокий странник", count: 3 },
+  ];
+
+  return (
+    <>
+      <div className="container">
+        {films.map(item => (
+          <div key={item.id}>
+            <h2>{item.name}</h2>
+            <Stars count={item.count} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/components/Stars/Stars.tsx
+++ src/components/Stars/Stars.tsx
@@ -4,12 +4,16 @@ interface StarsProps {
   count: number;
 }
 
-const Stars: React.FC<StarsProps> = ({ count }) => {
-  if (typeof count !== 'number' || count < 1 || count > 5) {
+const Stars: React.FC<StarsProps> = ({ count = 0}) => {
+  if (typeof count !== "number" || count < 1 || count > 5) {
     return null;
   }
 
-  const starsArray = Array.from({ length: count }, (_, index) => <Star key={index} />);
+  const starsArray = Array.from({ length: count }, (_, index) => (
+    <li key={index}>
+      <Star />
+    </li>
+  ));
 
   return (
     <ul className="card-body-stars u-clearfix">
@@ -19,4 +23,3 @@ const Stars: React.FC<StarsProps> = ({ count }) => {
 };
 
 export default Stars;
-
